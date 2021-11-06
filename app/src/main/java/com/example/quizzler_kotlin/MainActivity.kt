5ef410188f2a65016b5f8861737b2e52bd1ded24
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2021-11-05 19:29:40 -0600

    Initial commit

---
+++ app/src/main/java/com/example/quizzler_kotlin/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.quizzler_kotlin
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val startButton : Button = findViewById(R.id.start_button)
+        startButton.setOnClickListener {
+            startActivity(Intent(this, Quizzler::class.java))
+        }
+    }
+}
