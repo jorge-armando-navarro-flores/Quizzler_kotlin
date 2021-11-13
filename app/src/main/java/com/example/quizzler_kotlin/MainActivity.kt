17a31d7ffed70cae3f29937835ee14e1967997ed
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2021-11-13 15:13:35 -0600

    spinner1

--- app/src/main/java/com/example/quizzler_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/quizzler_kotlin/MainActivity.kt
@@ -3,24 +3,35 @@ package com.example.quizzler_kotlin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Button
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.runBlocking
-import org.json.JSONArray
-import java.util.concurrent.ExecutionException
+import android.widget.*
 
 class MainActivity : AppCompatActivity() {
+    lateinit var spinner: Spinner
+    lateinit var adapter: ArrayAdapter<String>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val startButton : Button = findViewById(R.id.start_button)
+        val startButton : Button = findViewById(R.id.startButton)
         startButton.setOnClickListener {
             val intent: Intent = Intent(this, QuizzlerActivity::class.java)
             startActivity(intent)
+        }
+        var categories = arrayListOf<String>("Python", "Java", "C++")
+        adapter = ArrayAdapter(applicationContext, android.R.layout.simple_spinner_dropdown_item, categories)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner = findViewById(R.id.categorySpinner)
+        spinner.adapter = adapter
+        spinner.onItemSelectedListener = object : AdapterView.OnItemSelectedListener{
+            override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+                Toast.makeText(applicationContext, "You selected " + adapter.getItem(position),Toast.LENGTH_SHORT).show()
+            }
+
+            override fun onNothingSelected(p0: AdapterView<*>?) {
+                TODO("Not yet implemented")
+            }
+
         }
     }
 
