cf2c8b07722893213b9c547000efb84be0824709
jorge-armando-navarro-flores <jorge.armando.navarro.flores@gmail.com>
2021-11-13 13:59:39 -0600

    true_false_pressed

--- app/src/main/java/com/example/quizzler_kotlin/QuizzlerActivity.kt
+++ app/src/main/java/com/example/quizzler_kotlin/QuizzlerActivity.kt
@@ -1,36 +1,75 @@
 package com.example.quizzler_kotlin
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
-import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import kotlinx.coroutines.GlobalScope
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
 import org.json.JSONArray
+import java.util.*
 import java.util.concurrent.ExecutionException
+import kotlin.concurrent.schedule
 
 class QuizzlerActivity : AppCompatActivity() {
     var questionBank: List<Question>? = null
-    var quiBrain: QuizBrain? = null
+    var quiz: QuizBrain? = null
     var questionTextView: TextView? = null
+    var scoreTextView: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quizzler)
         questionBank = getData(null)
-        quiBrain = QuizBrain(questionBank!!)
+        quiz = QuizBrain(questionBank!!)
+        scoreTextView = findViewById(R.id.scoreLabel)
         questionTextView = findViewById<TextView>(R.id.questionArea)
-        getNextQuestion(null)
+        getNextQuestion()
 //        Log.d("CREATIOOOOOOOOOOOON", questionBank!![0].text)
         questionTextView = findViewById<View>(R.id.questionArea) as TextView
     }
 
-    fun getNextQuestion(view: View?){
-        val qText = quiBrain?.nextQuestion()
-        questionTextView?.setText(qText)
+    fun getNextQuestion(){
+        questionTextView?.setBackgroundColor(resources.getColor(R.color.white))
+        scoreTextView?.setText("Score: ${quiz!!.score}")
+        if(quiz?.stillHasQuestions() == true){
+            val qText = quiz?.nextQuestion()
+            questionTextView?.setText(qText)
+        } else{
+            questionTextView?.setText("You've reached the end of the quiz.")
+
+        }
+
+    }
+
+    fun truePressed(view: View?){
+        val isRight: Boolean = quiz?.checkAnswer("True") == true
+        giveFeedback(isRight)
+    }
+
+    fun falsePressed(view: View?){
+        val isRight: Boolean = quiz?.checkAnswer("False") == true
+        giveFeedback(isRight)
+    }
+
+    fun giveFeedback(isRight: Boolean){
+        if(isRight){
+            questionTextView?.text = "Right"
+            questionTextView?.setBackgroundColor(resources.getColor(R.color.right))
+        } else{
+            questionTextView?.text = "Wrong"
+            questionTextView?.setBackgroundColor(resources.getColor(R.color.wrong))
+        }
+
+
+
+        Timer().schedule(2000) {
+            runOnUiThread {
+                getNextQuestion()
+            }
+        }
+
     }
 
     fun getData(view: View?): List<Question> {
